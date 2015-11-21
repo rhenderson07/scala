ac5d55839128af064100607d3249a1424960d1b3
rhenderson07 <rhenderson07@gmail.com>
2015-11-21 01:14:09 -0500

    Complete 8. create 9

---
+++ Euler/src/problems/Problem9.scala
@@ -0,0 +1,9 @@
+package problems
+
+object Problem9 extends Problem with App {
+  def number = 9
+  def description = "There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc"
+
+  def run = -1
+
+}
