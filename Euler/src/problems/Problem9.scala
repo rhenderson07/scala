f2abc4b3a57bb254b2f4a9188267b2fdd5e22d6b
rhenderson07 <rhenderson07@gmail.com>
2015-11-21 01:30:20 -0500

    complete 9

--- Euler/src/problems/Problem9.scala
+++ Euler/src/problems/Problem9.scala
@@ -4,6 +4,15 @@ object Problem9 extends Problem with App {
   def number = 9
   def description = "There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc"
 
-  def run = -1
+  def run = brute.last
 
+  lazy val target = 1000
+
+  def isTriplet(a: Int, b: Int, c: Int): Boolean = a * a + b * b == c * c
+
+  def brute = {
+    for (a <- 1 to target; b <- a to target; c <- b to target; if isTriplet(a, b, c) && a + b + c == 1000) yield a * b * c
+  }
+
+  println(run)
 }
