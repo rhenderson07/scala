df68cf4af2597d738028ed6981419e1653535e6a
rhenderson07 <rhenderson07@gmail.com>
2015-11-22 10:55:10 -0500

    Rename problems to fix order when sorted by name

--- Euler/src/problems/Problem001.scala
+++ Euler/src/problems/Problem001.scala
@@ -1,6 +1,6 @@
 package problems
 
-object Problem1 extends Problem {
+object Problem001 extends Problem {
   def number = 1
   def description = "Find the sum of all the multiples of 3 or 5 below 1000."
 
