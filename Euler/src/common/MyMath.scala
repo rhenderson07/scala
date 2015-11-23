dd937416364100b30db41bf29425b587963a14ed
rhenderson07 <rhenderson07@gmail.com>
2015-11-23 15:43:55 -0500

    Rename object to match file name

--- Euler/src/common/MyMath.scala
+++ Euler/src/common/MyMath.scala
@@ -1,6 +1,6 @@
 package common
 
-object Primes {
+object MyMath {
   lazy val primes: Stream[Long] = 2L #:: Stream.from(3).map(x => x.longValue).filter(i => primes.takeWhile(j => j * j <= i).forall(k => i % k != 0))
   
   def primeAt(n: Int): Long = primes.take(n).last
