a8ade379f795051201801514fb007e34616533ae
rhenderson07 <rhenderson07@gmail.com>
2015-11-24 01:00:14 -0500

    create 28

---
+++ Euler/src/problems/Problem028.scala
@@ -0,0 +1,15 @@
+
+package problems
+
+import scala.annotation.tailrec
+import common.MyMath
+
+object Problem028 extends Problem with App {
+  def number = 28
+  def description = "What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed by starting with the number 1 and moving to the right in a clockwise direction?"
+  def run = -1
+  
+  def numSquare(dimensionSize : Int) = {
+    
+  }
+}
