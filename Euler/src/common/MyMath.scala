239cc4214bbaa8044e2e9b61ee76d370e47babd4
rhenderson07 <rhenderson07@gmail.com>
2015-11-24 17:25:34 -0500

    Complete 28

--- Euler/src/common/MyMath.scala
+++ Euler/src/common/MyMath.scala
@@ -4,16 +4,20 @@ object MyMath {
   val primes: Stream[Long] = 2L #:: Stream.from(3).map(_.longValue).filter(!divisibleByPrime(_))
   def divisibleByPrime(n: Long): Boolean = primes.takeWhile(i => i * i <= n).exists(n % _ == 0)
   def primeAt(n: Int): Long = primes.take(n).last
-    
-  
-  def factorial(n:Int): BigInt = (1 to n).map(BigInt(_)).reduce(_*_)
-  
+
+  def factorial(n: Int): BigInt = (1 to n).map(BigInt(_)).reduce(_ * _)
+
   // does the same as the above factorial function, with a more concise syntax.
-  private def factorial2(n:Int): BigInt = (BigInt(1) /: (1 to n)) (_*_)
-  
+  private def factorial2(n: Int): BigInt = (BigInt(1) /: (1 to n))(_ * _)
+
   /**
    * Sum the individual digits of the provided number
    */
-  def asListOfDigits(n :BigInt): List[Int] = n.toString.toList.map(_.asDigit)
+  def asListOfDigits(n: BigInt): List[Int] = n.toString.toList.map(_.asDigit)
 
+  // integer square root. does not have rounding errors
+  def intSqrt(n: Int): Int = Stream.from(0).takeWhile(x => x * x <= n).last
+  
+  // return the next sq
+  def intSqrtRoundUp(n: Int): Int = Stream.from(0).find(x => x * x >= n).get
 }
--- Euler/src/problems/Problem028.scala
+++ Euler/src/problems/Problem028.scala
@@ -1,15 +1,57 @@
 
 package problems
 
-import scala.annotation.tailrec
 import common.MyMath
 
 object Problem028 extends Problem with App {
   def number = 28
   def description = "What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed by starting with the number 1 and moving to the right in a clockwise direction?"
   def run = -1
-  
-  def numSquare(dimensionSize : Int) = {
-    
+
+  val squareEdgeLen = 1001
+
+  class Point(val x: Int, val y: Int) {
+    override def toString() = {
+      "[%d,%d]".format(x, y)
+    }
   }
+
+  def findLocation(value: Int): Point = {
+    val lastRoot = MyMath.intSqrt(value - 1)
+    val lastSquare = lastRoot * lastRoot
+
+    val nextOrCurrentRoot = MyMath.intSqrtRoundUp(value)
+    val nextOrCurrentSquare = nextOrCurrentRoot * nextOrCurrentRoot
+
+    val lastRootEven = lastRoot % 2 == 0
+    val halfWayToNextSquare = lastSquare + (nextOrCurrentSquare - lastSquare) / 2
+    val underHalfWayToNextRoot = value <= halfWayToNextSquare
+
+    if (lastRootEven && underHalfWayToNextRoot)
+      //up
+      new Point(-lastRoot / 2, -lastRoot / 2 + (halfWayToNextSquare - value))
+    else if (lastRootEven && !underHalfWayToNextRoot)
+      //right
+      new Point(-lastRoot / 2 + (value - halfWayToNextSquare) - 1, lastRoot / 2)
+    else if (!lastRootEven && underHalfWayToNextRoot)
+      //down
+      new Point(lastRoot / 2 + 1, (halfWayToNextSquare - value) - lastRoot / 2)
+    else
+      //left
+      new Point(lastRoot / 2 + 2 + (halfWayToNextSquare - value), -lastRoot / 2 - 1)
+  }
+
+  def numSpiral(dimensionSize: Int) = {
+    val dimSquare = dimensionSize * dimensionSize
+    (1 to dimSquare).map(x => (findLocation(x), x))
+  }
+
+  def spriralDiagSum(n: Int): Long = numSpiral(n).filter(x => isDiagonal(x._1)).map(_._2.longValue()).sum
+
+  def isDiagonal(p: Point): Boolean = {
+    Math.abs(p.x) == Math.abs(p.y)
+  }
+
+  println(numSpiral(5))
+  println(spriralDiagSum(5))
 }
