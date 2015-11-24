54cfd8699e344f0c8db36b7a0aa362c44f0e4bf9
rhenderson07 <rhenderson07@gmail.com>
2015-11-24 00:25:06 -0500

    delete remnant Primes class

--- Euler/src/problems/Problem007.scala
+++ Euler/src/problems/Problem007.scala
@@ -1,11 +1,11 @@
 package problems
-import common.Primes
+import common.MyMath
 
 object Problem007 extends Problem with App {
   def number = 7
   def description = "What is the 10 001st prime number?"
 
-  def run = Primes.primeAt(10001)
+  def run = MyMath.primeAt(10001)
 
   println(run)
 }
