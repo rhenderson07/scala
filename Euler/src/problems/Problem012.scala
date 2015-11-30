2755b3d11c51773252e959635df16e1b808d5648
rhenderson07 <rhenderson07@gmail.com>
2015-11-30 10:57:44 -0500

    Problem 12 - Add scanLeft Iterator, to avoid memory limitations

--- Euler/src/problems/Problem012.scala
+++ Euler/src/problems/Problem012.scala
@@ -14,27 +14,28 @@ object Problem012 extends Problem with App {
   // attempt to reuse stream. runs slightly faster than triangle 1, still does not terminate for value 100,000
   lazy val triangles2: Stream[Long] = 1L #:: Stream.from(2).map(x => triangles2(x - 2) + x)
 
-  // attempt with iteration. Blazingly fast. still runs out of memory for value 10,000,000
+  // attempt with iteration. Blazingly fast. Runs out of heap space for value 10,000,000
   def incrementTriangle(pair: (Int, Long)) = (pair._1 + 1, pair._1 + 1 + pair._2)
   lazy val trianglePairs: Stream[(Int, Long)] = Stream.iterate((1, 1L))(incrementTriangle)
   lazy val triangles3: Stream[Long] = trianglePairs.map(_._2)
 
   //fourth attempt, using scan left
-  lazy val triangles4 = Stream.from(2).scanLeft(1)(_ + _)
-  
+  lazy val triangles4 = Stream.from(2).map(_.longValue).toIterator.scanLeft(1L)(_ + _)
+
   // first attempt. takes too long to find divisors. does not terminate
   def firstTriangleWithMinDivisors(minDivisors: Int): (Int, Long) = {
     trianglePairs.find(x => MyMath.findDivisors(x._2).length > minDivisors).get
   }
 
-  // second attempt. Uses fast method to count divisors. Runs instantly
+  // second attempt. Uses prime method to count divisors. Runs instantly
   def firstTriangleWithMinDivisors2(minDivisors: Int) = {
     val triangleDivisors = trianglePairs.map(x => MyMath.divisorCount(x._2))
     trianglePairs.zip(triangleDivisors).find(x => x._2 > minDivisors).get
   }
-  
-  //println(run)
-  val testVal = 100000000
-  println(MyMath.divisorCount(testVal))
-  println(MyMath.findDivisors(testVal).length)
+
+  // Third attempt. Uses scanLeft to find triangle numbers. Uses prime method to count divisors.
+  def firstTriangleWithMinDivisors3(minDivisors: Int) = {
+  }
+
+  println(run)
 }
