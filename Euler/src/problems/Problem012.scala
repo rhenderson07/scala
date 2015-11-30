3d19861b4d40c046e8937a1f26de7892bf6c4d2b
rhenderson07 <rhenderson07@gmail.com>
2015-11-29 22:02:21 -0500

    Problem 12, use scanLeft to find triangle numbers

--- Euler/src/problems/Problem012.scala
+++ Euler/src/problems/Problem012.scala
@@ -11,7 +11,7 @@ object Problem012 extends Problem with App {
   // first attempt, using range to calculate a sum at each item in the stream. Very slow
   lazy val triangles: Stream[Long] = Stream.from(1).map(x => (1L to x.longValue()).fold(0L)(_ + _))
 
-  // attempt to reuse stream. runs slightly faster than triangle 1, still does terminate for value 100000
+  // attempt to reuse stream. runs slightly faster than triangle 1, still does not terminate for value 100,000
   lazy val triangles2: Stream[Long] = 1L #:: Stream.from(2).map(x => triangles2(x - 2) + x)
 
   // attempt with iteration. Blazingly fast. still runs out of memory for value 10,000,000
@@ -19,7 +19,8 @@ object Problem012 extends Problem with App {
   lazy val trianglePairs: Stream[(Int, Long)] = Stream.iterate((1, 1L))(incrementTriangle)
   lazy val triangles3: Stream[Long] = trianglePairs.map(_._2)
 
-  //TODO attempt using scan left
+  //fourth attempt, using scan left
+  lazy val triangles4 = Stream.from(2).scanLeft(1)(_ + _)
   
   // first attempt. takes too long to find divisors. does not terminate
   def firstTriangleWithMinDivisors(minDivisors: Int): (Int, Long) = {
