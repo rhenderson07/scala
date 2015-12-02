7237bd25b12dbbd8999068f581c82c546b9e6b0d
rhenderson07 <rhenderson07@gmail.com>
2015-12-02 11:19:42 -0500

    Problem1 - extend App

--- Euler/src/problems/Problem001.scala
+++ Euler/src/problems/Problem001.scala
@@ -1,8 +1,10 @@
 package problems
 
-object Problem001 extends Problem {
+object Problem001 extends App {
   def number = 1
   def description = "Find the sum of all the multiples of 3 or 5 below 1000."
 
-  def run = (0 until 1000).filter((x: Int) => x % 3 == 0 || x % 5 == 0).reduce(_ + _)
+  def run = (0 until 1000).par.filter((x: Int) => x % 3 == 0 || x % 5 == 0).reduce(_ + _)
+  
+  println(run)
 }
