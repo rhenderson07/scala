2cbae2022abe09df6ad1bb8d8e7f97674ab82d9f
rhenderson07 <rhenderson07@gmail.com>
2015-12-07 20:29:13 -0500

    several more attempts to solve problem 60

--- Euler/src/common/Lists.scala
+++ Euler/src/common/Lists.scala
@@ -4,14 +4,25 @@ import scala.annotation.tailrec
 
 object Lists {
   @tailrec
-  def holdsForFamily[T <: Any](property: (T, T) => Boolean)(candList: List[T]): Boolean = {
+  def holdsForFamily3[T <: Any](property: (T, T) => Boolean)(candList: List[T]): Boolean = {
     if (candList.length <= 1)
       true
     else if (candList.tail.forall(property(_, candList.head)))
-      holdsForFamily(property)(candList.tail)
+      holdsForFamily3(property)(candList.tail)
     else
       false
   }
+
+  def holdsForFamily[T <: Any](property: (T, T) => Boolean)(candList: Stream[T]): Boolean = {
+    candList.forall(x => candList.filter(_ != x).forall(y => property(x, y)))
+  }
+
+  /**
+   * returns an iterator/stream of all subsets with the defined length
+   */
+//  def subSets[T <: Any](length: Int): List[List[T]] = {
+//    
+//  }
 
   // TODO define this in a common list function
   def containsMonotonic[T <: Long](l: List[T])(n: T): Boolean = {
