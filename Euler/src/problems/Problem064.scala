72bce81ef4dc3ff81ba99799f29dbf21e39ababf
rhenderson07 <rhenderson07@gmail.com>
2015-12-09 15:30:33 -0500

    update Problem 1 to use cleaner syntax. Create Problem 64

---
+++ Euler/src/problems/Problem064.scala
@@ -0,0 +1,10 @@
+
+package problems
+
+object Problem064 extends Problem with App {
+  def number = 64
+  def description = "How many continued fractions for N <= 10000 have an odd period?"
+  lazy val run = -1L
+  
+  println(run)
+}
