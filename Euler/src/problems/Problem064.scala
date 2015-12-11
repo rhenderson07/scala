563237766a9781625dffa67908dd908450e6e938
rhenderson07 <rhenderson07@gmail.com>
2015-12-11 10:15:24 -0500

    Problem 64 complete

--- Euler/src/problems/Problem064.scala
+++ Euler/src/problems/Problem064.scala
@@ -12,11 +12,15 @@ object Problem064 extends Problem with App {
 
   lazy val Target = 10000
 
-  def oddPeriodCount(upperBound: Int): Int = (1 to upperBound).map(rootContinuedFraction).count(_.period % 2 == 1)
+  // My Solution: about 200 seconds. Most time is spent finding the square root
+  def oddPeriodCount(upperBound: Int): Int = (1 to upperBound).par.map(rootContinuedFraction).count(_.period % 2 == 1)
 
   def rootContinuedFraction(n: Int): ContinuedFraction = {
-    val BigDecPrecision = 200
-    val root = MyMath.bigSqrt(BigDecPrecision)(n)
+    val BasePrecision = 150
+    val PrecisionModifierConstant = 30
+    val sqRtPrecision = BasePrecision + n / PrecisionModifierConstant
+
+    val root = MyMath.bigSqrt(sqRtPrecision)(n)
     ContinuedFractionGenerator.asContinuedFraction(root)
   }
 
@@ -26,5 +30,21 @@ object Problem064 extends Problem with App {
   //println(rootContinuedFraction(4846))
 
   //test for range
-  (1 to 10000).foreach(x => println(x, rootContinuedFraction(x)))
+  //  (1 to 10000).par.foreach(x => println(x, rootContinuedFraction(x)))
+
+  // From Euler. About 1 second
+  // defined at https://projecteuler.net/thread=64;page=7
+  def getNums(n: Int, t: Int, d: Int, pastResults: Set[(Int, Int, Int)], aggResult: List[Int]): List[Int] = {
+    val d2 = (n - t * t) / d
+    val n2 = ((Math.sqrt(n) - t) * 1 / d2).toInt
+    val t2 = -t - d2 * n2
+    if (pastResults.contains(n2, t2, d2)) aggResult
+    else getNums(n, t2, d2, pastResults + ((n2, t2, d2)), aggResult :+ n2)
+  }
+
+  def oddPeriod(x: Int) = !Math.sqrt(x).isWhole() && getNums(x, 0, 1, Set(), List()).length % 2 == 1
+
+  val res = (1 to 10000).count(oddPeriod)
+  println(res)
+
 }
