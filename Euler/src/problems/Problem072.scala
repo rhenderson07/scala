ac6db733ebffd560780170493dc0a3bbd7e33efb
rhenderson07 <rhenderson07@gmail.com>
2015-12-14 16:46:44 -0500

    Problem 72 begin Farey series solution

--- Euler/src/problems/Problem072.scala
+++ Euler/src/problems/Problem072.scala
@@ -100,5 +100,28 @@ object Problem072 extends Problem with App {
     result
   }
 
+  def fifth(n: Int) = {
+    //    def farey( n, asc=True ):
+    //    """Python function to print the nth Farey sequence, either ascending or descending."""
+    //    if asc: 
+    //        a, b, c, d = 0, 1,  1 , n     # (*)
+    //    else:
+    //        a, b, c, d = 1, 1, n-1, n     # (*)
+    //    print "%d/%d" % (a,b)
+    //    while (asc and c <= n) or (not asc and a > 0):
+    //        k = int((n + b)/d)
+    //        a, b, c, d = c, d, k*c - a, k*d - b
+    //        print "%d/%d" % (a,b)
+    def farey(n: Int) = {
+      def rec(a: Int, b: Int, c: Int, d: Int): (Int, Int) = {
+        (-1, -1)
+      }
+
+      rec(0, 1, 1, n)
+    }
+
+    farey(n)
+  }
+
   println(run)
 }
