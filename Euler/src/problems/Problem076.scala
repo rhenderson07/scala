5f76146b30949a7c6f435fb231e384282267dcbf
rhenderson07 <rhenderson07@gmail.com>
2015-12-17 11:03:54 -0500

    Creation of Problem and Test class

---
+++ Euler/src/problems/Problem076.scala
@@ -0,0 +1,17 @@
+package problems
+import common.math.Primes
+
+object Problem076 extends Problem with App {
+  def number = 76
+  def description = "How many different ways can one hundred be written as a sum of at least two positive integers?"
+
+  def run = -1L
+
+  def summations(n: Int) = {
+   
+  }
+  
+  time(summations)(5)
+  
+  (5 to 0 by -1).foreach(println)
+}
