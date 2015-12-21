a2b6392654a2e111cb66fde5cc1739c2adfaeaf8
rhenderson07 <rhenderson07@gmail.com>
2015-12-21 18:24:08 -0500

    Problem 84: produce output assuming all squares are normal.

--- Euler/src/problems/Problem084.scala
+++ Euler/src/problems/Problem084.scala
@@ -9,7 +9,7 @@ object Problem084 extends Problem with App {
 
   lazy val run = -1L
 
-  val monopolyCircuit = Seq("GO", "A1", "CC1", "A2", "T1", "R1", "B1", "CH1", "B2", "B3", "JAIL",
+  val monopolyCircuit = List("GO", "A1", "CC1", "A2", "T1", "R1", "B1", "CH1", "B2", "B3", "JAIL",
     "C1", "U1", "C2", "C3", "R2", "D1", "CC2", "D2", "D3", "FP", "E1", "CH2", "E2", "E3", "R3",
     "F1", "F2", "U2", "F3", "G2J", "G1", "G2", "CC3", "G3", "R4", "CH3", "H1", "T2", "H2");
 
@@ -21,9 +21,9 @@ object Problem084 extends Problem with App {
     val filler = List.fill[String](TotalCount - special.size)("")
     filler ::: special
   }
-  
+
   val chanceCards = {
-    
+
   }
 
   // enum for ca
@@ -33,30 +33,44 @@ object Problem084 extends Problem with App {
   //  case class Chance(val name:String) extends Square;
 
   // enum for ca
-//  abstract class SquareType extends Enumeration {
-//    type SquareType = Value
-//    val BUILDING, CHANCE, COMMUNITY, RAILROAD, UTILITY, GO, JAIL, GOTOJAIL = Value
-//  }
-//
-//  val chanceCards = {
-//    val TotalCount = 16
-//    val special = List("GO", "JAIL", "")
-//    List.fill(TotalCount - special.size)("") :: special
-//  }
+  //  abstract class SquareType extends Enumeration {
+  //    type SquareType = Value
+  //    val BUILDING, CHANCE, COMMUNITY, RAILROAD, UTILITY, GO, JAIL, GOTOJAIL = Value
+  //  }
+  //
+  //  val chanceCards = {
+  //    val TotalCount = 16
+  //    val special = List("GO", "JAIL", "")
+  //    List.fill(TotalCount - special.size)("") :: special
+  //  }
 
   //Planning:
   // I think the best way to go about this is to look at each position and calculate for each other pos,
   // what are the odds of ending up at the target pos, from there.
 
-  //Dice.diceFeq(List(6, 6, 6, 6)).foreach(println)
-  communityChestCards.take(50).foreach(println)
+  // given a square and assuming all squares are normal, what are the possible outcomes and their corresponding probability?
+  def outcomesForSquare(list: List[String])(rolls: List[Int])(index: Int) = {
+    rolls.map(x => list((x + index) % list.size))
+  }
+
+  // Dice.diceFeq(List(6, 6, 6, 6)).foreach(println)
+  // communityChestCards.take(50).foreach(println)
+  lazy val test = {
+    val rolls = Dice.diceOutcomes(List(6, 6))
+    val outcomeFunct = outcomesForSquare(monopolyCircuit)(rolls) _
+    val outcomes = (0 until monopolyCircuit.size).map(outcomeFunct).flatten.toList
+
+    Dice.frequency(outcomes)
+  }
+
+  test.foreach(println)
 }
 
 object Dice {
   /**
    * List of possible outcomes for a given set of die
    */
-  private def diceOutcomes(dice: List[Int]) = {
+  def diceOutcomes(dice: List[Int]) = {
     def rec(remainingDice: List[Int], outcomes: List[Int]): List[Int] = {
       if (remainingDice.isEmpty) {
         outcomes
@@ -72,7 +86,7 @@ object Dice {
   /**
    * Return a list of frequencies for each element in the list
    */
-  private def frequency[T <% Ordered[T]](l: List[T]) = {
+  def frequency[T <% Ordered[T]](l: List[T]) = {
     val totalCount = l.size
     l.groupBy(identity).mapValues(_.size * BigDecimal(100) / totalCount).toList.sortBy(_._1)
   }
