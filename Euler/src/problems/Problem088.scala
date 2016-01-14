8473177c94f29364ab58814731f5751855da0fbe
rhenderson07 <rhenderson07@gmail.com>
2016-01-14 15:55:44 -0500

    Problem 88: mod to improve performance. Still too slow to use.

--- Euler/src/problems/Problem088.scala
+++ Euler/src/problems/Problem088.scala
@@ -8,42 +8,121 @@ import scala.annotation.tailrec
 object Problem088 extends Problem with App {
   def number = 88
   def description = "What is the sum of all the minimal product-sum numbers for 2 =< k =< 12000?"
-
   def run = -1L
 
-  /**
-   * For given value k, returns minimal sum product composed of that many elements
+  /*
+   * Math insight to help solve problem:
+   * 
+   * The first insight we should get is that if we have a set of factors say {2,3,4} we get that 2*3*4 = 24
+   * and 2+3+4 = 9. Something that isn’t exactly a product-sum like we are looking for. However, we can add
+   *  ones which doesn’t change the product but change the sum. Indeed if we add 24-9 = 15 ones we will get
+   *  a product sum with 18 factors. In other words any set of factors can be converted into a product-sum
+   *  for k = “product of factor” – “sum of factors” + “number of factors”. Whether this is minimal or not is
+   *  another question which we wont answer just yet.
+   *  
+   *  The second insight we might get is that the minimal product-sum for k is larger than or equal to k
+   *  since the minimal product-sum consist of k ones which sums to k. The upper limit for the minimal
+   *  product sum for k is 2k. The reason is that we can always use the factors {2,k} this gives us the
+   *  product 2k and the sum 2+k. By adding k-2 ones we end up with a valid product sum for k which equals
+   *  2k. In other words the minimal product-sum (mps) for k is k =< mps(k) =< 2k.
+   *  
+   *  With these two insight we should be able to find the solution for the problem.
    */
-  def minimalSumProduct(k: Int): Int = {
-    Stream.from(1).map(i => sumProduct(k)(i)).find(_.nonEmpty).get.get.sum
-  }
 
-  def isSumProduct(l: List[Int]) = {
-    l.sum == l.product
-  }
+  // second attempt. using insights
+  {
+    val maxK = 12000
+    val maxNumber = 2 * maxK
 
-  /**
-   * For the given value x, find all sum product sets
-   */
-  def sumProduct(length: Int)(x: Int): Option[List[Int]] = {
-    // get divisors in reverse order
-    val divisors = MyMath.findDivisors(x).sorted.map(_.toInt)
+    val numFactors = (Math.log(maxNumber) / Math.log(2)).intValue
+    val factors = Array.fill(numFactors)(0)
 
-    comb(length, divisors).find(isSumProduct)
-  }
+    val k = (0 to maxK).map(x => x * 2).toArray;
+    k(1) = 0;
 
-  private def combInner[T](n: Int, l: List[T]): List[List[T]] =
-    n match {
-      case 0 => List(List())
-      case _ => for (
-        element <- l;
-        sl <- combInner(n - 1, l.dropWhile(_ != element))
-      ) yield element :: sl
+    factors(0) = 1;
+    val curMaxFactor = 1;
+    val j = 0;
+
+    def factorization2(max: Int) = {
+      for {
+        // all factorizations of length 2
+        i <- 2 to max / 2
+        j <- i to max / i
+      } yield (i, j)
     }
 
-  def comb[T](n: Int, l: List[T]): List[List[T]] = combInner(n, l.distinct)
+    def factorization3(max: Int) = {
+      for {
+        // all factorizations of length 3
+        i1 <- 2 to max / 2
+        i2 <- i1 to max / i1
+        i3 <- i2 to max / i1 / i2
+      } yield (i1, i2, i3)
+    }
 
-  //  MyMath.findDivisors(156).sorted.reverse.combinations(3).foreach(println)
-  //  sumProduct(2)(2).foreach(println)
-  println(minimalSumProduct(6))
+    def factorization4(max: Int) = {
+      for {
+        // all factorizations of length 3
+        i1 <- 2 to max / 2
+        i2 <- i1 to max / i1
+        i3 <- i2 to max / i1 / i2
+        i4 <- i3 to max / i1 / i2 / i3
+      } yield (i1, i2, i3, i4)
+    }
+
+    def factorization14(max: Int) = {
+      for {
+        // all factorizations of length 3
+        i1 <- 2 to max / 2
+        i2 <- i1 to max / i1
+        i3 <- i2 to max / i1 / i2
+        i4 <- i3 to max / i1 / i2 / i3
+      } yield (i1, i2, i3, i4)
+    }
+
+    println("starting")
+    factorization2(24000)
+    factorization3(24000)
+    factorization4(24000)
+    println("done")
+  }
+
+  //first attempt. Very clunky
+  {
+    /**
+     * For given value k, returns minimal sum product composed of that many elements
+     */
+    def minimalSumProduct(k: Int): Int = {
+      Stream.from(1).map(i => sumProduct(k)(i)).find(_.nonEmpty).get.get.sum
+    }
+
+    def isSumProduct(l: List[Int]) = {
+      l.sum == l.product
+    }
+
+    /**
+     * For the given value x, find all sum product sets
+     */
+    def sumProduct(length: Int)(x: Int): Option[List[Int]] = {
+      // get divisors in reverse order
+      val divisors = MyMath.findDivisors(x).sorted.map(_.toInt)
+      comb(length, divisors).find(isSumProduct)
+    }
+
+    def combInner[T](n: Int, l: List[T]): List[List[T]] =
+      n match {
+        case 0 => List(List())
+        case _ => for (
+          element <- l;
+          sl <- combInner(n - 1, l.dropWhile(_ != element))
+        ) yield element :: sl
+      }
+
+    def comb[T](n: Int, l: List[T]): List[List[T]] = combInner(n, l.distinct)
+
+    //  MyMath.findDivisors(156).sorted.reverse.combinations(3).foreach(println)
+    //  sumProduct(2)(2).foreach(println)
+    //println(minimalSumProduct(6))
+  }
 }
--- Euler/test/problems/Problem088Suite.scala
+++ Euler/test/problems/Problem088Suite.scala
@@ -7,18 +7,18 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class Problem088Suite extends FunSuite {
 
-  test("isSumProduct (2,2) is true") {
-    val elements = List(2, 2)
-    assert(Problem088.isSumProduct(elements))
-  }
-
-  test("isSumProduct (1,1,2,4) is true") {
-    val elements = List(1, 1, 2, 4)
-    assert(Problem088.isSumProduct(elements))
-  }
-
-  test("isSumProduct (1,5) is false") {
-    val elements = List(1, 5)
-    assert(!Problem088.isSumProduct(elements))
-  }
+//  test("isSumProduct (2,2) is true") {
+//    val elements = List(2, 2)
+//    assert(Problem088.isSumProduct(elements))
+//  }
+//
+//  test("isSumProduct (1,1,2,4) is true") {
+//    val elements = List(1, 1, 2, 4)
+//    assert(Problem088.isSumProduct(elements))
+//  }
+//
+//  test("isSumProduct (1,5) is false") {
+//    val elements = List(1, 5)
+//    assert(!Problem088.isSumProduct(elements))
+//  }
 }
